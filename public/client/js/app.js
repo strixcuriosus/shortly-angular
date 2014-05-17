9b6ac3e492e395441910e429107954fd658d3733
Hack Reactor Students <strixcuriosus@gmail.com>
2014-05-16 20:16:19 -0700

    Set up ng-view template

--- public/client/js/app.js
+++ public/client/js/app.js
@@ -1,4 +1,17 @@
 angular.module('shortly', [])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    controller: 'linkController',
+    template: '<div id="container"><div class="links" ng-controller="linkController"><div class="link" ng-repeat="link in links">
+  <img src="/redirect_icon.png">
+  <div class="info">
+    <div class="visits"><span class="count">{{link.visits}}</span>Visits</div>
+    <div class="title">{{link.title}}</div>
+    <div class="original">{{link.url}}</div>
+    <a href="http://localhost:4568/738dd">{{link.base_url}}</a>
+  </div>
+</div></div></div>'
+  })
 .controller('linkController', function($scope, $http){
   $http({
   method: 'GET',
